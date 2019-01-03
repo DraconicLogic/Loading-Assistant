34b276b352f2cd2fc2595e87e428b2e852a52c52
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2019-01-03 09:11:36 +0000

    setup layout of stack section component

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -8,9 +8,24 @@ class Stack extends Component {
   }
   render() {
     return (
-      <div id="Stack">
-        <div>Stack Viewer</div>
-        <div>Stack Options</div>
+      <div id="stack-section">
+        <div id="stack-editor">
+          <div className="stack-editor__bale" id="bale-one"></div>
+          <div className="stack-editor__bale" id="bale-two"></div>
+          <div className="stack-editor__bale" id="bale-three"></div>
+          <div className="stack-editor__bale" id="bale-four"></div>
+          <div className="stack-editor__bale" id="bale-five"></div>
+          <div className="stack-editor__bale" id="bale-six"></div>
+          <div className="stack-editor__bale" id="bale-seven"></div>
+          <div className="stack-editor__bale" id="bale-eight"></div>
+          <div className="stack-editor__bale" id="bale-nine"></div>
+          <div className="stack-editor__bale" id="bale-ten"></div>
+          <div className="stack-editor__bale" id="bale-eleven"></div>
+          <div className="stack-editor__bale" id="bale-twelve"></div>
+        </div>
+        <div id="stack-options">
+          Stack Options
+        </div>
       </div>
     );
   }
