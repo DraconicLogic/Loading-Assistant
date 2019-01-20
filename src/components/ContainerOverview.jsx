0df65d636d0e59d644843deb6db52dfb9d6500c9
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2019-01-20 15:50:46 +0000

    Arrange stack editor bales in logical order

---
+++ src/components/ContainerOverview.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ContainerOverview = ({container}) => {
+  const flatContainer = container.flat(1)
+  console.log(flatContainer)
+  const baleCount = flatContainer.reduce((tallyObj, bale) => {
+    if (!tallyObj[bale.code]) {
+      tallyObj[bale.code] = 1
+    } else if (!!tallyObj[bale.code]) {
+      tallyObj[bale.code] += 1
+    }
+    return tallyObj
+  },{})
+  const baleCountArray = Object.entries(baleCount)
+  console.log(baleCount)
+  
+  return (
+    <div>
+      <h1>Container Overview</h1>
+      <table>
+        {baleCountArray.map((bale) => {
+          return (
+            <tr>
+              <td>{bale[0]}</td>
+              <td>{bale[1]}</td>
+            </tr>
+          )
+        })}
+      </table>
+    </div>
+  );
+};
+
+export default ContainerOverview;
--- src/components/StackEditor.jsx
+++ src/components/StackEditor.jsx
@@ -8,18 +8,18 @@ class StackEditor extends Component {
     return (
       <div id="stack-section">
         <div id="stack-editor">
-          <div className="stack-editor__bale" id="bale-1">{stack[0] ? stack[0].code: null}</div>
-          <div className="stack-editor__bale" id="bale-2">{stack[1] ? stack[1].code: null}</div>
-          <div className="stack-editor__bale" id="bale-3">{stack[2] ? stack[2].code: null}</div>
-          <div className="stack-editor__bale" id="bale-4">{stack[3] ? stack[3].code: null}</div>
-          <div className="stack-editor__bale" id="bale-5">{stack[4] ? stack[4].code: null}</div>
-          <div className="stack-editor__bale" id="bale-6">{stack[5] ? stack[5].code: null}</div>
-          <div className="stack-editor__bale" id="bale-7">{stack[6] ? stack[6].code: null}</div>
-          <div className="stack-editor__bale" id="bale-8">{stack[7] ? stack[7].code: null}</div>
-          <div className="stack-editor__bale" id="bale-9">{stack[8] ? stack[8].code: null}</div>
-          <div className="stack-editor__bale" id="bale-10">{stack[9] ? stack[9].code: null}</div>
-          <div className="stack-editor__bale" id="bale-11">{stack[10] ? stack[10].code: null}</div>
-          <div className="stack-editor__bale" id="bale-12">{stack[11] ? stack[11].code: null}</div>
+          <div className="stack-editor__bale" id="stack-editor__bale-1">{stack[0] ? stack[0].code: null}</div>
+          <div className="stack-editor__bale" id="stack-editor__bale-2">{stack[1] ? stack[1].code: null}</div>
+          <div className="stack-editor__bale" id="stack-editor__bale-3">{stack[2] ? stack[2].code: null}</div>
+          <div className="stack-editor__bale" id="stack-editor__bale-4">{stack[3] ? stack[3].code: null}</div>
+          <div className="stack-editor__bale" id="stack-editor__bale-5">{stack[4] ? stack[4].code: null}</div>
+          <div className="stack-editor__bale" id="stack-editor__bale-6">{stack[5] ? stack[5].code: null}</div>
+          <div className="stack-editor__bale" id="stack-editor__bale-7">{stack[6] ? stack[6].code: null}</div>
+          <div className="stack-editor__bale" id="stack-editor__bale-8">{stack[7] ? stack[7].code: null}</div>
+          <div className="stack-editor__bale" id="stack-editor__bale-9">{stack[8] ? stack[8].code: null}</div>
+          <div className="stack-editor__bale" id="stack-editor__bale-10">{stack[9] ? stack[9].code: null}</div>
+          <div className="stack-editor__bale" id="stack-editor__bale-11">{stack[10] ? stack[10].code: null}</div>
+          <div className="stack-editor__bale" id="stack-editor__bale-12">{stack[11] ? stack[11].code: null}</div>
         </div>
         <div id="stack-options">
           Stack Options
