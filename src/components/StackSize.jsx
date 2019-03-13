7675ffdc272e01ef5bb5d756ab55a355b4dec4c9
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2019-03-13 04:54:18 +0000

    add 9 to stackSize component

--- src/components/StackSize.jsx
+++ src/components/StackSize.jsx
@@ -5,6 +5,7 @@ const StackSize = ({size}) => {
   return (
     <div>
       <button onClick={() => size(12)}>12</button>
+      <button onClick={() => size(9)}>9</button>
       <button onClick={() => size(6)}>6</button>
       <button onClick={() => size(3)}>3</button>
       <button onClick={() => size(1)}>1</button>
