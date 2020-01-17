22a7f377c8a88096c80b2d63f5a59920691b97e2
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2020-01-17 00:15:19 +0000

    improve ux of stack size buttons

--- src/components/StackSize.jsx
+++ src/components/StackSize.jsx
@@ -3,11 +3,21 @@ import React from "react";
 const StackSize = ({ size }) => {
   return (
     <div id="stack-section__size">
-      <button onClick={() => size(12)}>12</button>
-      <button onClick={() => size(9)}>9</button>
-      <button onClick={() => size(6)}>6</button>
-      <button onClick={() => size(3)}>3</button>
-      <button onClick={() => size(1)}>1</button>
+      <button className="stack-section__size__button" onClick={() => size(12)}>
+        12
+      </button>
+      <button className="stack-section__size__button" onClick={() => size(9)}>
+        9
+      </button>
+      <button className="stack-section__size__button" onClick={() => size(6)}>
+        6
+      </button>
+      <button className="stack-section__size__button" onClick={() => size(3)}>
+        3
+      </button>
+      <button className="stack-section__size__button" onClick={() => size(1)}>
+        1
+      </button>
     </div>
   );
 };
