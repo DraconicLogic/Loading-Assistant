bcc499d604e4fdc51013f5d6b22fcbc5c3c9a88d
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2020-06-30 22:19:22 +0100

    Add extra info popover for container weight in status bar
    Tapping the weight item in the status bar brings up a small modal where there is extra information

--- src/components/StatusBar.jsx
+++ src/components/StatusBar.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import products from "../products/products.json";
 import PropTypes from "prop-types";
+import { Popover } from "@material-ui/core";
 
 const StatusBar = ({ content, date }) => {
+  const [extraInfo, setExtraInfo] = useState(false);
+
   const flattenedContent = content
     .reduce((flattened, stackObj) => {
       flattened.push(stackObj.stackContent);
@@ -22,16 +25,30 @@ const StatusBar = ({ content, date }) => {
     color: "red",
   };
 
+  const toggleExtraInfo = () => {
+    setExtraInfo(!extraInfo);
+  };
+
   return (
     <div id="status-bar">
       <span className="status-bar__item">Row: {rowNumber + 1}</span>
       <span className="status-bar__item">Bales: {totalBales}</span>
       <span
+        onClick={toggleExtraInfo}
         className="status-bar__item"
-        style={netWeight > 26000 ? overWeightStyle : null}
+        id="status-bar__weight"
+        style={netWeight > 26500 ? overWeightStyle : null}
       >
         Weight: {netWeight}kg
       </span>
+      <Popover
+        open={extraInfo}
+        anchorEl={document.getElementById("status-bar__weight")}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        onClose={toggleExtraInfo}
+      >
+        Space Remaining: {26500 - netWeight}kg
+      </Popover>
       <span className="status-bar__item">Date: {date}</span>
     </div>
   );
