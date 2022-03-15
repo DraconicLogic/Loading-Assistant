8893a64d81458d531f8d582f2ba385899f595e1a
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2022-03-15 05:50:48 +0000

    remove redundant imports in StatusBar.jsx

--- src/components/StatusBar/StatusBar.jsx
+++ src/components/StatusBar/StatusBar.jsx
@@ -1,80 +1,81 @@
 import React, { useState } from "react";
 import products from "../../products/products.json";
 import PropTypes from "prop-types";
-import SyncStatus from "../SyncStatus/SyncStatus.jsx"
-import SyncIcon from '@material-ui/icons/Sync';
-import DoneIcon from '@material-ui/icons/Done';
+import SyncStatus from "../SyncStatus/SyncStatus.jsx";
 import { Popover } from "@material-ui/core";
 import dayjs from "dayjs";
 
 const StatusBar = ({ content, date, synced }) => {
-  const [extraInfo, setExtraInfo] = useState(false);
+	const [extraInfo, setExtraInfo] = useState(false);
 
-  const flattenedContent = content
-    .reduce((flattened, stackObj) => {
-      flattened.push(stackObj.stackContent);
-      return flattened;
-    }, [])
-    .flat(1);
+	const flattenedContent = content
+		.reduce((flattened, stackObj) => {
+			flattened.push(stackObj.stackContent);
+			return flattened;
+		}, [])
+		.flat(1);
 
-  const rowNumber = Math.floor(content.length / 6);
-  const totalBales = flattenedContent.length;
+	const rowNumber = Math.floor(content.length / 6);
+	const totalBales = flattenedContent.length;
 
-  const netWeight = flattenedContent.reduce((weight, item) => {
-    return (weight += products[item].size);
-  }, 0);
+	const netWeight = flattenedContent.reduce((weight, item) => {
+		return (weight += products[item].size);
+	}, 0);
 
-  const overWeightStyle = {
-    color: "red",
-  };
+	const overWeightStyle = {
+		color: "red",
+	};
 
-  const nearMaxWeightStyle = {
-    color: "orangered"
-  }
+	const nearMaxWeightStyle = {
+		color: "orangered",
+	};
 
-  const numberInRow = (content.length) % 6
-  
-  const toggleExtraInfo = () => {
-    setExtraInfo(!extraInfo);
-  };
+	const numberInRow = content.length % 6;
 
-  return (
-    <div id="status-bar">
-      <span className="status-bar__item">Row: {rowNumber + 1}<br /> {numberInRow}/6 </span>
-      <span className="status-bar__item">Bales: {totalBales}</span>
-      <span
-        onClick={toggleExtraInfo}
-        className="status-bar__item"
-        id="status-bar__weight"
-        style={
-          (netWeight > 26500)
-           ? overWeightStyle 
-           : (netWeight >= 26000 && netWeight <= 26500) 
-           ? nearMaxWeightStyle 
-           : null
-        }
-      >
-        Weight: {netWeight}kg
-      </span>
-      <Popover
-        open={extraInfo}
-        anchorEl={document.getElementById("status-bar__weight")}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-        onClose={toggleExtraInfo}
-      >
-        Space Remaining: {26500 - netWeight}kg
-      </Popover>
-      <span className="status-bar__item">
-        <SyncStatus synced={synced}/>
-      </span>
-      <span className="status-bar__item">Date: {dayjs(date).format('DD/MM/YYYY')}</span>
-    </div>
-  );
+	const toggleExtraInfo = () => {
+		setExtraInfo(!extraInfo);
+	};
+
+	return (
+		<div id="status-bar">
+			<span className="status-bar__item">
+				Row: {rowNumber + 1}
+				<br /> {numberInRow}/6{" "}
+			</span>
+			<span className="status-bar__item">Bales: {totalBales}</span>
+			<span
+				onClick={toggleExtraInfo}
+				className="status-bar__item"
+				id="status-bar__weight"
+				style={
+					netWeight > 26500
+						? overWeightStyle
+						: netWeight >= 26000 && netWeight <= 26500
+						? nearMaxWeightStyle
+						: null
+				}>
+				Weight: {netWeight}kg
+			</span>
+			<Popover
+				open={extraInfo}
+				anchorEl={document.getElementById("status-bar__weight")}
+				anchorOrigin={{ vertical: "top", horizontal: "center" }}
+				onClose={toggleExtraInfo}>
+				Space Remaining: {26500 - netWeight}kg
+			</Popover>
+			<span className="status-bar__item">
+				<SyncStatus synced={synced} />
+			</span>
+			<span className="status-bar__item">
+				Date: {dayjs(date).format("DD/MM/YYYY")}
+			</span>
+		</div>
+	);
 };
 
 StatusBar.propTypes = {
-  content: PropTypes.arrayOf(PropTypes.object),
-  date: PropTypes.string,
+	content: PropTypes.arrayOf(PropTypes.object),
+	date: PropTypes.string,
 };
 
 export default StatusBar;
