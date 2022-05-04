8d4983854925c4c5890266d39fa95feeefbe6999
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2022-05-04 06:03:05 +0100

    Change innerHtml text of product button for better UI

--- src/components/ProductButton/ProductButton.jsx
+++ src/components/ProductButton/ProductButton.jsx
@@ -2,21 +2,33 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const ProductButton = ({ product, add }) => {
-  const handleClick = (event) => {
-    const { value } = event.target;
-    add(value);
-  };
+	const handleClick = (event) => {
+		const { value } = event.target;
+		add(value);
+	};
 
-  return (
-    <button id="product-button" onClick={handleClick} value={product}>
-      {product}
-    </button>
-  );
+	function transformText(text) {
+		const glossary = {
+			PARTYDRESS: "PARTY DRESS",
+			SCHUNI: "SCH UNI",
+			WHAT: "W/S HAT",
+			"L POLO D": "L-POLO-D",
+			LEATHER: "LTHER",
+			BLANKET: "BLNKT",
+		};
+		return glossary[text] ? glossary[text] : text;
+	}
+
+	return (
+		<button className="product-button" onClick={handleClick} value={product}>
+			{transformText(product)}
+		</button>
+	);
 };
 
 ProductButton.propTypes = {
-  product: PropTypes.string.isRequired,
-  add: PropTypes.func,
+	product: PropTypes.string.isRequired,
+	add: PropTypes.func,
 };
 
 export default ProductButton;
