da30bb65889dc6ef81d44e0a9c21bf96a64bb51e
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2023-06-14 07:05:23 +0100

    Implement Search Product Feature

--- src/views/ProductList/ProductList.jsx
+++ src/views/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import products from "../../products/products.json";
 import ProductButton from "../../components/ProductButton/ProductButton.jsx";
 import StackEditor from "../../components/StackEditor/StackEditor.jsx";
@@ -11,110 +11,44 @@ import SaveSharpIcon from "@material-ui/icons/SaveSharp";
 import { TextField } from "@material-ui/core";
 import PropTypes from "prop-types";
 
-// convert to a functional component
-class ProductList extends Component {
-	state = {
-		stackPosition: 0,
-		currentStack: Array(12),
-		bales: [],
-	};
+function ProductList({ savedStacks, handleSaveStack, handleAddToContainer }) {
+	const [stackPosition, setStackPosition] = useState(0);
+	const [currentStack, setCurrentStack] = useState(Array(12));
+	const [bales, setBales] = useState([]);
 
-	componentDidMount() {
-		// bales state should be populated when the component is mounted
+	useEffect(() => {
+		const productCodes = Object.keys(products).sort();
+		console.log("ProductCodes: ", productCodes);
+		setBales(productCodes);
+	}, []);
+
+	useEffect(() => {
+		highlightNextPosition();
+	}, [currentStack]);
+
+	function handleSearch(event) {
+		// TODO: Implement this handler
+		console.log("filter products");
 	}
 
-	render() {
-		const { stackPosition, currentStack } = this.state;
-		const productCodes = Object.keys(products);
-
-		const bales = productCodes.sort();
-
-		const handleSearch = (event) => {
-			console.log("filter products");
-		};
-
-		return (
-			<div id="product-list" className="App__view">
-				<div id="product-list__search">
-					<TextField
-						label="Search Products"
-						variant="filled"
-						type="search"
-						onChange={handleSearch}
-					/>
-				</div>
-				<div id="product-list__buttons">
-					{bales.map((bale) => {
-						return (
-							<ProductButton add={this.addToStack} product={bale} key={bale} />
-						);
-					})}
-				</div>
-				<div id="stack-section">
-					<StackSize size={this.toggleStackSize} />
-					<span id="cancel-button">
-						<CancelIcon onClick={() => this.clearStack()} />
-					</span>
-
-					<StackEditor
-						stack={currentStack}
-						position={stackPosition}
-						mark={this.markPosition}
-						context="editor"
-					/>
-					<div id="stack-options--1">
-						<button
-							className="stack-options__button"
-							onClick={this.handleAddToDB}
-							data-testid="save-to-DB">
-							<SaveSharpIcon>Save for Later</SaveSharpIcon>
-						</button>
-					</div>
-					<div id="stack-options--2">
-						<button
-							className="stack-options__button"
-							onClick={this.handleAddContainer}>
-							<LocalShippingIcon />
-						</button>
-					</div>
-				</div>
-			</div>
-		);
+	function markPosition(marker) {
+		setStackPosition(marker);
 	}
 
-	markPosition = (marker) => {
-		this.setState({
-			stackPosition: marker,
-		});
-	};
-
-	addToStack = (baleCode) => {
-		const { currentStack, stackPosition } = this.state;
-
+	function addToStack(baleCode) {
 		if (currentStack.length <= 12) {
 			const newStack = [...currentStack];
 			newStack[stackPosition] = baleCode;
-			this.setState(
-				{
-					currentStack: newStack,
-				},
-				() => this.highlightNextPosition()
-			);
+			setCurrentStack(newStack);
 		}
-	};
+	}
 
-	highlightNextPosition = () => {
-		const { currentStack } = this.state;
+	function highlightNextPosition() {
 		const emptyPosition = utils.findEmptyPosition(currentStack);
-		this.setState({
-			stackPosition: emptyPosition,
-		});
-	};
+		setStackPosition(emptyPosition);
+	}
 
-	handleAddContainer = () => {
-		const { handleAddToContainer, savedStacks } = this.props;
-		const { currentStack } = this.state;
-
+	function handleAddContainer() {
 		let isStackFilled = true;
 		for (let i = 0; i < currentStack.length; i++) {
 			if (currentStack[i] === undefined) {
@@ -129,29 +63,17 @@ class ProductList extends Component {
 			};
 
 			handleAddToContainer(newStack);
-			this.clearStack();
+			clearStack();
 		} else {
 			alert("Please fill in the stack");
 		}
-	};
+	}
 
-	clearStack = () => {
-		const { currentStack } = this.state;
-		this.setState({
-			currentStack: Array(currentStack.length),
-			stackPosition: 0,
-		});
-	};
+	function toggleStackSize(size) {
+		setCurrentStack(Array(size));
+	}
 
-	toggleStackSize = (size) => {
-		this.setState({
-			currentStack: Array(size),
-		});
-	};
-
-	handleAddToDB = () => {
-		const { currentStack } = this.state;
-		const { handleSaveStack, savedStacks } = this.props;
+	function handleAddToDB() {
 		let isStackFilled = true;
 		for (let i = 0; i < currentStack.length; i++) {
 			if (currentStack[i] === undefined) {
@@ -166,11 +88,62 @@ class ProductList extends Component {
 				date,
 			};
 			handleSaveStack(stackObj);
-			this.clearStack();
+			clearStack();
 		} else {
 			alert("Please fill in the stack");
 		}
-	};
+	}
+
+	function clearStack() {
+		setCurrentStack(Array(currentStack.length));
+		setStackPosition(0);
+	}
+
+	return (
+		<div id="product-list" className="App__view">
+			<div id="product-list__search">
+				<TextField
+					label="Search Products"
+					variant="filled"
+					type="search"
+					onChange={handleSearch}
+				/>
+			</div>
+			<div id="product-list__buttons">
+				{bales.map((bale) => {
+					return <ProductButton add={addToStack} product={bale} key={bale} />;
+				})}
+			</div>
+			<div id="stack-section">
+				<StackSize size={toggleStackSize} />
+				<span id="cancel-button">
+					<CancelIcon onClick={() => clearStack()} />
+				</span>
+
+				<StackEditor
+					stack={currentStack}
+					position={stackPosition}
+					mark={markPosition}
+					context="editor"
+				/>
+				<div id="stack-options--1">
+					<button
+						className="stack-options__button"
+						onClick={handleAddToDB}
+						data-testid="save-to-DB">
+						<SaveSharpIcon>Save for Later</SaveSharpIcon>
+					</button>
+				</div>
+				<div id="stack-options--2">
+					<button
+						className="stack-options__button"
+						onClick={handleAddContainer}>
+						<LocalShippingIcon />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 ProductList.propTypes = {
